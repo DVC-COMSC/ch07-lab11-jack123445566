37c38a69480617f6c3dfc5f936176be7f5a6ebca
jack123445566 <112512759+jack123445566@users.noreply.github.com>
2022-10-20 01:41:37 +0000

    1st commit

--- main.py
+++ main.py
@@ -2,15 +2,5 @@ numbers = [     [10, 11, 12, 13, 14],
                [20, 21, 22, 23, 24],
                [30, 31, 32, 33, 34]]
 
-# numbers =[] 
-# rnum = int(input('Enter the number of rows'))
-# for i in range(rnum):
-# 	row = list(map(int, input('Enter 5 values for a row: ').split()))
-# 	numbers.append(row)
-
-rnum = len(numbers)
-cnum = len(numbers[0])
-
-# ******************************
-# Make your Code
-# ******************************
+summation=[sum(i) for i in numbers]
+print (summation)
